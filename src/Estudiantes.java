121dfaeba22aefd2d2d63953a3a24f98715da166
franciscocaero <150805792+franciscocaero@users.noreply.github.com>
2023-11-24 11:36:12 -0500

    Crear clase

--- src/Estudiantes.java
+++ src/Estudiantes.java
@@ -1,2 +1,110 @@
-package PACKAGE_NAME;public class Estudiantes {
+public class Estudiantes {
+    public String nombre;
+    public String carrera;
+    public int id;
+    public String nomMateria1;
+    public double materia1;
+    public String nomMateria2;
+    public double materia2;
+    public String nomMateria3;
+    public double materia3;
+    public String nomMateria4;
+    public double materia4;
+    public String nomMateria5;
+    public double materia5;
+    public Estudiantes(String nuevoNombre, int nuevoId){
+        nombre=nuevoNombre;
+        id=nuevoId;
+    }
+    public String getNombre() {
+        return nombre;
+    }
+    public String getCarrera() {
+        return carrera;
+    }
+    public int getId() {
+        return id;
+    }
+    public String getNomMateria1() {
+        return nomMateria1;
+    }
+    public double getMateria1() {
+        return materia1;
+    }
+    public String getNomMateria2() {
+        return nomMateria2;
+    }
+    public double getMateria2() {
+        return materia2;
+    }
+    public String getNomMateria3() {
+        return nomMateria3;
+    }
+    public double getMateria3() {
+        return materia3;
+    }
+    public String getNomMateria4() {
+        return nomMateria4;
+    }
+    public double getMateria4() {
+        return materia4;
+    }
+    public String getNomMateria5() {
+        return nomMateria5;
+    }
+    public double getMateria5() {
+        return materia5;
+    }
+
+    public void setNombre(String newNombre) {
+        this.nombre = newNombre;
+    }
+
+    public void setCarrera(String newCarrera) {
+        this.carrera = newCarrera;
+    }
+
+    public void setId(int newId) {
+        this.id = newId;
+    }
+
+    public void setNomMateria1(String newNomMateria1) {
+        this.nomMateria1 = newNomMateria1;
+    }
+
+    public void setMateria1(double newMateria1) {
+        this.materia1 = newMateria1;
+    }
+
+    public void setNomMateria2(String newNomMateria2) {
+        this.nomMateria2 = newNomMateria2;
+    }
+
+    public void setMateria2(double newMateria2) {
+        this.materia2 = newMateria2;
+    }
+
+    public void setNomMateria3(String newNomMateria3) {
+        this.nomMateria3 = newNomMateria3;
+    }
+
+    public void setMateria3(double newMateria3) {
+        this.materia3 = newMateria3;
+    }
+
+    public void setNomMateria4(String newNomMateria4) {
+        this.nomMateria4 = newNomMateria4;
+    }
+
+    public void setMateria4(double newMateria4) {
+        this.materia4 = newMateria4;
+    }
+
+    public void setNomMateria5(String newNomMateria5) {
+        this.nomMateria5 = newNomMateria5;
+    }
+
+    public void setMateria5(double newMateria5) {
+        this.materia5 = newMateria5;
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,14 @@
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Estudiantes miEstudiante;
+        Estudiantes miEstudiante2;
+        Estudiantes miEstudiante3;
+        miEstudiante=new Estudiantes("Francisco",36541);
+        miEstudiante2=new Estudiantes("Juan",12345);
+        miEstudiante3=new Estudiantes("Marta",87541);
+        Scanner miVariable=new Scanner(System.in);
+
+
     }
 }
