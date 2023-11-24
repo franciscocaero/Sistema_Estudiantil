0c3e14e7aad7749018b4a9f55b220af0fdb4cbfd
franciscocaero <150805792+franciscocaero@users.noreply.github.com>
2023-11-24 12:08:05 -0500

    + materias

--- src/Main.java
+++ src/Main.java
@@ -22,6 +22,17 @@ public class Main {
         miEstudiante.setMateria1(notaFinal);
         System.out.println("Nota final de "+miEstudiante.getNomMateria1()+" : "+ miEstudiante.getMateria1());
         System.out.println(miEstudiante.promedio1(miEstudiante.materia1));
+        System.out.println("Ingrese el nombre de la segunda materia: ");
+        String materia2=miVariable.nextLine();
+        miEstudiante.setNomMateria2(materia2);
+        System.out.println("Ingrese la nota del primer bimestre de la materia: "+miEstudiante.getNomMateria2());
+        nota1=miVariable2.nextDouble();
+        System.out.println("Ingrese la nota del segundo bimestre de la materia: "+miEstudiante.getNomMateria2());
+        nota2=miVariable3.nextDouble();
+        notaFinal=(nota1+nota2)/2;
+        miEstudiante.setMateria2(notaFinal);
+        System.out.println("Nota final de "+miEstudiante.getNomMateria2()+" : "+ miEstudiante.getMateria2());
+        System.out.println(miEstudiante.promedio2(miEstudiante.materia2));
 
     }
 }
